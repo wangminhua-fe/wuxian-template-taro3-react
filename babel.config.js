4e9a7127760a2165482a3b16b196981b54e42ae9
Marshall Wang <wmhayt228@gmail.com>
2023-12-16 17:20:45 +0800

    feat(ui): 新增nutui组件库

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Marshall
+ * @Date: 2023-12-16 01:11:46
+ * @LastEditors: Marshall
+ * @LastEditTime: 2023-12-16 17:10:48
+ * @Description:
+ * @FilePath: /wuxian-template-taro3-react/babel.config.js
+ */
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 module.exports = {
@@ -6,5 +14,17 @@ module.exports = {
       framework: 'react',
       ts: true
     }]
+  ],
+  plugins: [
+    [
+      "import",
+      {
+        "libraryName": "@nutui/nutui-react-taro",
+        "libraryDirectory": "dist/esm",
+        "style": 'css',
+        "camel2DashComponentName": false
+      },
+      'nutui-react-taro'
+    ]
   ]
 }
--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Marshall
  * @Date: 2023-12-16 01:40:41
  * @LastEditors: Marshall
- * @LastEditTime: 2023-12-16 16:59:39
+ * @LastEditTime: 2023-12-16 17:19:41
  * @Description:
  * @FilePath: /wuxian-template-taro3-react/src/pages/welcome/index.tsx
  */
@@ -11,6 +11,8 @@ import { useDidShow, useLoad } from '@tarojs/taro'
 import DemoService from '@/services/demo.service'
 import './index.scss'
 import { useState } from 'react'
+import { Button } from '@nutui/nutui-react-taro';
+import { Add } from '@nutui/icons-react-taro';
 
 export default function Index() {
 
@@ -28,6 +30,8 @@ export default function Index() {
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+      <Add color='red' />
+      <Button type='primary'>主要按钮</Button>
       <View>接口请求结果：{content}</View>
     </View>
   )
